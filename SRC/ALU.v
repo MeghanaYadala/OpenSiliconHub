51840c8bd52965ea6c966542c0604b6b8cb4e528
ABHILASH MADDINENI <214151262+MrAbhi19@users.noreply.github.com>
2025-11-15 22:28:56 +0530

    Update ALU.v

--- SRC/ALU.v
+++ SRC/ALU.v
@@ -10,12 +10,13 @@ module ALU #(
     case(sel)
       3'b000: out=A+B;
       3'b001: out=A-B;
-      3'b010: out=A|B;
-      3'b011: out=A&B;
-      3'b100: out=A^B;
-      3'b101: out=~(A|B);
-      3'b110: out=~(A&B);
-      3'b111: out=~(A^B);
+      3'b010: out={1'b0,A|B};
+      3'b011: out={1'b0,A&B};
+      3'b100: out={1'b0,A^B};
+      3'b101: out={1'b0,~(A|B)};
+      3'b110: out={1'b0,~(A&B)};
+      3'b111: out={1'b0,~(A^B)};
+      default: out={SIZE+1{1'b0}};
     endcase
   end
 endmodule
