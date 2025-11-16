a304a444428ecb273f200536fb9db4c426b65d79
ABHILASH MADDINENI <214151262+MrAbhi19@users.noreply.github.com>
2025-11-16 11:21:27 +0530

    Update UART_TX.v

--- SRC/UART_TX.v
+++ SRC/UART_TX.v
@@ -1,14 +1,16 @@
-module UART_TX(clk,reset,tx_start,data,tx_line,tx_busy,tx_done);
+module UART_TX#(
   parameter clk_freq=50000000;
   parameter baud_rate=9600;
-
+)(
   input wire clk,reset,tx_start;
   input wire [7:0] data;
   output reg tx_line,tx_busy,tx_done;
+);
+  
   localparam clks_per_bit=clk_freq/baud_rate;
   reg [15:0] clk_count;
   reg [3:0] bit_index;
-  reg [9:0] s_reg=10'b1111111111;
+  reg [9:0] s_reg<=10'b1111111111;
 
   always@(posedge clk or posedge reset) begin
     if (reset) begin
@@ -19,7 +21,7 @@ module UART_TX(clk,reset,tx_start,data,tx_line,tx_busy,tx_done);
       tx_done<=0;
     end else begin
       if (tx_start && !tx_busy) begin
-        s_reg={1'b1,data,1'b0};
+        s_reg<={1'b1,data,1'b0};
         tx_busy<=1;
         clk_count<=0;
         bit_index<=0;
